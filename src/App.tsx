e47d3f3a4f0122d3558550c14c1d10c162114376
Camille Janin <cmille.jn@gmail.com>
2025-02-12 14:18:01 +0100

    test color

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="read-the-docs" style={{color : "red"}}>
         Click on the Vite and React logos to learn more
       </p>
     </>
